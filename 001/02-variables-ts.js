aa4fe13264c18668adcda332852bbfe60e8902b0
Aiman <danielo14ch@hotmail.com>
2019-10-22 08:42:32 -0500

    primitivas

--- 001/02-variables-ts.js
+++ 001/02-variables-ts.js
@@ -1,20 +1,17 @@
 // variables primitivas 
-const numero = 10
-const decimal = 10.12
-const respuestaTipoNumero = typeof numero; 
-console.log(respuestaTipoNumero)
+const numero = 10;
+const decimal = 10.12;
+const respuestaTipoNumero = typeof numero;
+console.log(respuestaTipoNumero);
 console.log(typeof decimal);
 const letra = "a";
-console.log(typeof letra)
+console.log(typeof letra);
 const palabra = "palabra";
-console.log(typeof palabra)
+console.log(typeof palabra);
 const hijos = null;
 console.log(typeof hijos);
-const casado = false
+const casado = false;
 console.log(typeof casado);
-const fecha = new Date();
-console.log(typeof fecha);
-
 //Gestor de Paquetes 
 // de node js -> comunicarse con el 
 // Repositorio -> Descarga el "paquete"
