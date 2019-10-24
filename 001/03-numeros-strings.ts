f7d87b3954f4fb3f0e119780112f4b8f64ccbef1
Aiman <danielo14ch@hotmail.com>
2019-10-24 00:09:28 -0500

    aritmetica basica

---
+++ 001/03-numeros-strings.ts
@@ -0,0 +1,17 @@
+
+// suma
+//resultado = 1 + 2;
+//restar 
+//resultado = 1 - 2;
+//multiplicación
+//resultado = 1 * 2;
+//dividir
+//resultado = 1/2;
+//exponencial
+//resultado = 1^2;
+// MOD 
+//resultado = 4 % 2; //0 -> residuo
+//resultado = 4 % 3; //1 -> residuo
+//resultado = 4 % 3; //1 -> residuo
+console.log("pera" + 2);
+
