a06d4b1d5a8e56a9ee5dbbb130c36b4c0ebb5a8c
Aiman <danielo14ch@hotmail.com>
2019-10-30 21:52:08 -0500

    condicionales 2.0

---
+++ 001/calculadora-bailable.ts
@@ -0,0 +1,14 @@
+const numero01 = prompt("ingresa el numero 1");
+const numero02 = prompt("ingresa el numero 2");
+const opcion   = prompt("ingrese para: sumar [A]; restar [B]; Multiplicar [C]; Dividir [D] ");
+ 
+const numeroc = Number(numero01);
+const numeroa = Number()
+
+if( opcion == "A"){
+    console.log(numero01 + numero02);
+}else{ if(opcion == "B"){
+    console.log(numero01 - nuemro02);
+
+}
+}
