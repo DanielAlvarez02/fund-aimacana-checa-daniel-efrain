3cb498e5cbc48c7df8dbcc048a50d11e41655529
Aiman <danielo14ch@hotmail.com>
2019-10-30 22:30:36 -0500

    calculadora 2.0

--- 001/calculadora-bailable.ts
+++ 001/calculadora-bailable.ts
@@ -1,14 +1,20 @@
 const numero01 = prompt("ingresa el numero 1");
 const numero02 = prompt("ingresa el numero 2");
-const opcion   = prompt("ingrese para: sumar [A]; restar [B]; Multiplicar [C]; Dividir [D] ");
+const opcion   = prompt("ingrese para: sumar [A]; restar [B]; Multiplicar [C]; Dividir [D]; Exponencial[E]");
  
 const numeroc = Number(numero01);
-const numeroa = Number()
+const numeroa = Number(numero02);
 
 if( opcion == "A"){
-    console.log(numero01 + numero02);
-}else{ if(opcion == "B"){
-    console.log(numero01 - nuemro02);
+    console.log(numeroc + numeroa);
+}else if(opcion == "B"){
+    console.log(numeroc - numeroa);
+}else if(opcion == "C"){
+    console.log(numeroc * numeroa);
+}else if(opcion == "D"){
+    console.log(numeroc % numeroa);
+}else if(opcion == "E"){
+    console.log( Math.exp(numeroc) ); 
+    console.log( Math.exp(numeroa) );
+}else console.log("");
 
-}
-}
