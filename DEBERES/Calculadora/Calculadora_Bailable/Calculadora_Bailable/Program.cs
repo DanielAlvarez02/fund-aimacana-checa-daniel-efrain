b81bef4adef949c1e9d7b590a5762fb4e8791b60
Aiman <danielo14ch@hotmail.com>
2019-11-04 17:17:19 -0500

    calculadora bailable en c#

---
+++ DEBERES/Calculadora/Calculadora_Bailable/Calculadora_Bailable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Calculadora_Bailable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora con operaciones básicas y función exponencial
+
+            Console.WriteLine("ingrese el primer numero, por favor");
+            double numero01 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero porfavor");
+            double numero02 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese [A] si desea Sumar; [B] si desea Restar; [C] si desea Multiplicar; [D] si desea Dividir; [E] si desea una Función Exponencial");
+            char opcion = Convert.ToChar(Console.ReadLine());
+            double euler = Math.E;
+
+
+            if (opcion == 'A')
+            {
+                Console.WriteLine(Convert.ToDouble(numero01 + numero02));
+            } else if (opcion == 'B')
+            {
+                Console.WriteLine(Convert.ToDouble(numero01 - numero02));
+            } else if (opcion == 'C')
+            {
+                Console.WriteLine(Convert.ToDouble(numero01 * numero02));
+            } else if (opcion == 'D')
+            {
+                Console.WriteLine(Convert.ToDouble(numero01 / numero02));
+            } else if (opcion == 'E')
+            {
+                Console.WriteLine(Convert.ToDouble(Math.Pow(euler, numero01)));
+                Console.WriteLine(Convert.ToDouble(Math.Pow(euler, numero02)));
+            } 
+
+        
+
+        }
+    }
+}
