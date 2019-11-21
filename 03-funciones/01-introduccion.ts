0a568d67f8025d1d37dded6bb9b0eb80a9d00f51
Davidrva <55715904+Davidrva@users.noreply.github.com>
2019-11-21 18:00:40 -0500

    funciones

---
+++ 03-funciones/01-introduccion.ts
@@ -0,0 +1,16 @@
+//FUNCION
+function log(
+    cualquierCosa //PARAMETRO
+    ){
+    console.log(cualquierCosa);
+}
+log("Hola Mundo");
+
+//otra funcion
+function sumarDosNumeros(
+    a : number,
+    b : number
+):number{
+    return a + b;
+}
+sumarDosNumeros(4, 2)
