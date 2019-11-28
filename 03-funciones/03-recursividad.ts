a2ec08d7d0f9e570b76571a4e8867d309c7a71fe
Daniel Aimacaña <daniel.aimacana@epn.edu.ec>
2019-11-28 16:50:53 -0500

    vectores

--- 03-funciones/03-recursividad.ts
+++ 03-funciones/03-recursividad.ts
@@ -31,3 +31,59 @@ function recorridoArreglo(
     }
 
 }
+
+
+
+
+const arregloDosDimensiones = [
+    [1,2],
+    [3,4]
+];
+
+arregloDosDimensiones[0][0]; //1
+arregloDosDimensiones[0][1]; //2
+
+
+const arregloMasDimensiones = [
+    
+        [1,2,3,4],
+        [4,5,6,7] //indice y longitud
+    
+];
+
+
+arregloMasDimensiones[0][0]; //1
+arregloMasDimensiones[0][1]; //2
+arregloMasDimensiones[1][0]; //3
+arregloMasDimensiones[1][1]; //4
+
+//Producto cruz
+//multiplicar el 0*n elemento +
+//multiplicar el 0 + 1*n - 1 elemnto +
+//multiplicar el 0 + 2*n - 2 elemento +
+
+arregloMasDimensiones[0][0]; //[1,2,3,4]
+arregloMasDimensiones[0].length; //4
+arregloMasDimensiones[1]; //[4,5,6,7]
+arregloMasDimensiones.length; //2
+
+function productoCruz(
+    arregloMasDimensiones: number[][]): number[]
+        {
+            let  longitud = arregloMasDimensiones[1].length
+            let i = 0;
+
+
+            for (i = 0, i < 4 , i++) {
+                let elemento = arregloMasDimensiones[0][i];
+                const elementos = elemento;
+
+                
+                
+            
+            }
+            
+}
+
+
+
