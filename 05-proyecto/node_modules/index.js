eaf5ace60ccb01a1f7bbb0fc8732bfa8c65c97f2
Daniel Aimacaña <daniel.aimacana@epn.edu.ec>
2020-01-06 16:07:47 -0500

    librerias

---
+++ 05-proyecto/node_modules/index.js
@@ -0,0 +1,8 @@
+//NODE MODULES
+//const sumaLibreria = require('libreria-2020/lib/suma');
+// IORTAR TODO EL CONTENIDO CON EL NOMBRE 'sumaLibreria'
+//del paquete 'libreria-2020/lib/suma'
+
+import * as sumaLibreria from 'libreria-2020/lib/suma';
+const respuesta = sumaLibreria(1,2);
+console.log('la respuest es: ', respuesta);
