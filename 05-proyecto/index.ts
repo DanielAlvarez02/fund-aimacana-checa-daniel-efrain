77e2c98cc4d424d27affc19c33e843029cbfceb1
daniel <daniel.aimacana@epn.edu.ec>
2020-01-06 19:48:44 -0500

    libreria 2.1.1

---
+++ 05-proyecto/index.ts
@@ -0,0 +1,7 @@
+//NODE MODULE
+// const sumaLibreria = require('libreria-2020/lib/suma');
+//Iportar todo el contenido con el nombre 'sumaLibreria'
+import * as sumaLibreria from 'libreria-2020/lib/suma';
+
+const respuesta = sumaLibreria(1,2);
+console.log('la respuesta es : ', respuesta);
--- 05-proyectos/node_modules/libreria-2020/lib/suma.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(numUno, numDos)
-{
-    return numUno + numDos;
-}
