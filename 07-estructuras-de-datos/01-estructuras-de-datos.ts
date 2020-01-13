97e429c0e2e11ea9b8995b3936f6e4b93efde075
Daniel Aimacaña <daniel.aimacana@epn.edu.ec>
2020-01-13 14:48:57 -0500

    estructuras de datos

---
+++ 07-estructuras-de-datos/01-estructuras-de-datos.ts
@@ -0,0 +1,11 @@
+//boolean
+//string
+//null -> object
+//number
+//persona
+//animal --> nombre  científico
+//fecha nacimiento, peso, altura, genero
+
+//casi en todos los lenguajes
+//struct -> Estructura de datos
+//clase -> estructuras de datos -> Metodos!
